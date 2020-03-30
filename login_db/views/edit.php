c9792f404f36a9c705a43c7de5b45433036cf243
jobairRafi96 <60178238+jobairRafi96@users.noreply.github.com>
2020-03-31 01:01:48 +0600

    edit user ui

--- login_db/views/edit.php
+++ login_db/views/edit.php
@@ -1,17 +1,38 @@
 <?php
+session_start();
 
-	$id = $_GET['id'];
-
-
+	$Id = $_GET['Id'];
+	$_SESSION['id']=$Id;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Edit</title>
 </head>
 <body>
+	<h2>Edit Page</h2>
+	<form action="../php/editcheck.php" method="post">
+		<table>
+			<tr>
+				<td>Username:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td>email:</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
 
 </body>
 </html>
