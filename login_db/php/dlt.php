770de4f42b93f15a1a0a5ba04dd91195503a2fc4
jobairRafi96 <60178238+jobairRafi96@users.noreply.github.com>
2020-03-30 15:55:56 +0600

    dlt

--- login_db/php/dlt.php
+++ login_db/php/dlt.php
@@ -1,21 +1,22 @@
 <?php
+	 	
 
 
-	if(isset($_POST['submit'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$email = $_POST['email'];
-		$type = $_POST['type'];
+		$id=$_GET['Id'];
+
+		echo $id;
 
 		$con = mysqli_connect('127.0.0.1', 'root', '', 'webtech');
 
-		$sql = "DELETE FROM users WHERE username='{$username}'";
+		$sql = "delete from users where Id='{$id}'";
 		if(mysqli_query($con, $sql)){
-			echo "Registration done!";
+			echo "delete  done!";
 		}else{
 			echo "Error";
 		}
-	}
+	
+
+?>
 	
 
 
